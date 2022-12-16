2c8dc0779084198aab611a430281b2ea45499c90
Elek <Elek2@yandex.ru>
2022-12-16 05:31:04 +0300

    Добавление базы данных

--- main.py
+++ main.py
@@ -2,35 +2,32 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from modules import create_tables
 import modules as M
+import json
 
 DSN = 'postgresql://postgres:nicaragua21@localhost:5432/omr'
 engine = sqlalchemy.create_engine(DSN)
 
+with open("tests_data.json", "r") as read_file:
+	data = json.load(read_file)
 
-# create_tables(engine)
+create_tables(engine)
 pub_name = input('Введите название издателя: ')
 
 with sessionmaker(bind=engine)() as session:
-	quer = session.query(M.Publisher.name, M.Shop.name, M.Sale.price, M.Sale.date_sale).\
-		join(M.Book).\
-		join(M.Stock).\
-		join(M.Shop).\
-		join(M.Sale).\
+	for i in data:
+		table_name = i['model'].capitalize()
+		table_colums = i['fields']
+		record = eval(f"M.{table_name}(**{table_colums})")
+		session.add(record)
+		session.commit()
+
+	quer = session.query(M.Publisher.name, M.Shop.name, M.Sale.price, M.Sale.date_sale). \
+		join(M.Book). \
+		join(M.Stock). \
+		join(M.Shop). \
+		join(M.Sale). \
 		filter(M.Publisher.name == f'{pub_name}').all()
 
 	for sale in quer:
 		print(*sale, sep=' | ')
 
-
-# 	course1 = Course(name='Python')
-# 	course2 = Course(name='Kotlin')
-# 	course3 = Course(name='JavaScript')
-# 	session.add_all([course1, course2, course3])
-#
-# 	hw1 = Homework(number=1, description='Первая ДЗ', course=course1)
-# 	hw2 = Homework(number=2, description='Вторая ДЗ', course=course3)
-# 	session.add_all([hw1, hw2])
-#
-# 	for i in session.query(Homework).all():
-# 		print(i)
-# 	session.commit()
--- modules.py
+++ modules.py
@@ -10,6 +10,9 @@ class Publisher(Table):
 	id = sq.Column(sq.Integer, primary_key=True)
 	name = sq.Column(sq.String, unique=True)
 
+	def __init__(self, name):
+		self.name = name
+
 
 class Book(Table):
 	__tablename__ = 'book'
@@ -19,12 +22,19 @@ class Book(Table):
 	id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"))
 	publisher = relationship(Publisher, backref="hz")
 
+	def __init__(self, title, id_publisher):
+		self.title = title
+		self.id_publisher = id_publisher
+
 
 class Shop(Table):
 	__tablename__ = 'shop'
 
 	id = sq.Column(sq.Integer, primary_key=True)
 	name = sq.Column(sq.String, unique=True)
+
+	def __init__(self, name):
+		self.name = name
 
 
 class Stock(Table):
@@ -37,22 +47,29 @@ class Stock(Table):
 	book = relationship(Book)
 	shop = relationship(Shop)
 
+	def __init__(self, id_book, id_shop, count):
+		self.id_book = id_book
+		self.id_shop = id_shop
+		self.count = count
+
 
 class Sale(Table):
 	__tablename__ = 'sale'
 
 	id = sq.Column(sq.Integer, primary_key=True)
-	price = sq.Column(sq.Integer, sq.CheckConstraint("price>0"))
+	price = sq.Column(sq.Float, sq.CheckConstraint("price>0"))
 	date_sale = sq.Column(sq.DATE, nullable=False)
 	id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
 	count = sq.Column(sq.Integer, nullable=False)
 	stock = relationship(Stock)
 
+	def __init__(self, price, date_sale, id_stock, count):
+		self.price = price
+		self.date_sale = date_sale
+		self.id_stock = id_stock
+		self.count = count
 
 
 def create_tables(engine):
 	Table.metadata.drop_all(engine)
 	Table.metadata.create_all(engine)
-
-
-
